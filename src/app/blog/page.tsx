10974013988b82123ae9707b72877db3aa716b35
aarochuk <andrewarochukwu@gmail.com>
2025-06-27 00:33:22 -0400

    blog page skeleton completed

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import BlogPage from '@/components/Blog/BlogPage';
+
+export default function Home() {
+  return (
+    <div className='body'>
+        <Navbar />
+      <BlogPage />
+      <Footer />
+    </div>
+  );
+}
