6c870a5061d9511ff1eaef526cfa678577de9683
aarochuk <andrewarochukwu@gmail.com>
2025-07-02 23:07:52 -0400

    next config file fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
