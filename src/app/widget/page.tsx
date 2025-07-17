1ed9f0d19ca836935bdc74c8e7dfb44e31ff34a9
aarochuk <andrewarochukwu@gmail.com>
2025-07-17 18:50:23 -0400

    widget component added directly to frontend

--- src/app/widget/page.tsx
+++ src/app/widget/page.tsx
@@ -2,17 +2,27 @@ import Navbar from '@/components/Navbar';
 import CTASection from '@/components/CTASection';
 import Footer from '@/components/Footer';
 import '../../styles/widget/page.css';
-import LocationWidget from '../../../../eign-widget/widget/src/components/LocationWidget';
+import Image from 'next/image';
+import LocationWidget from '@/components/LocationWidget';
 
 export default function Home() {
   return (
-    <div className='body'>
+    <div className='body bg-white'>
         <Navbar />
-        <form className='my-10 flex items-center justify-center gap-20'>
-          <input type='text' placeholder='Search for an address, neighborhood or city' />
-          <button>View Location</button>
+        <form className='my-10 flex items-center justify-center gap-10'>
+          <div className='search_holder'>
+            <Image
+              src="/images/widget/image.png"
+              alt="Company Logo"
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            <input type='text' placeholder='Search for an address, neighborhood or city' />
+          </div>
+          <button className='search_button'>View Location</button>
         </form>
-        <h1>252 Phillip Street, Waterloo, ON</h1>
+        <h1 className='my-10 flex justify-center items-center location_name'>252 Phillip Street, Waterloo, ON</h1>
         <LocationWidget location={'Sheikh Zayed Road (south), Dubai, United Arab Emirates'} />
         <CTASection />
         <Footer />
